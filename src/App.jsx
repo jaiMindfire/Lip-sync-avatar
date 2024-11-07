76aceac54c68152fa0e91a84488569cb5a305dc5
jaiMindfire <jaib@mindfiresolutions.com>
2024-11-07 18:58:28 +0530

    Adding jumping and Walking animations

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-      <color attach="background" args={["#ececec"]} />
+    <>
       <Experience />
-    </Canvas>
+    </>
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,19 +1,55 @@
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import {
+  CameraControls,
+  Environment,
+  Gltf,
+  OrbitControls,
+  useCursor,
+  useTexture,
+} from "@react-three/drei";
 import { Avatar } from "./Avatar";
-import { useThree } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Camera } from "three";
+import { degToRad } from "three/src/math/MathUtils.js";
+import { useState } from "react";
 
 export const Experience = () => {
-  const texture = useTexture("textures/classroom.jpg");
-  const viewport = useThree((state) => state.viewport);
+  const [pos, setPos] = useState({
+    x: null,
+    y: null,
+    z: null,
+  });
+
+  const [move, setMove] = useState(false);
+
+  const [onFloor, setOnFloor] = useState(false);
+
   return (
     <>
-      <OrbitControls />
-      <Avatar position={[0, -3, 5]} scale={2} />
-      <Environment preset="sunset" />
-      <mesh>
-        <planeGeometry args={[viewport.width, viewport.height]} />
-        <meshBasicMaterial map={texture} />
-      </mesh>
+      <Canvas shadows camera={{ position: [0, 0, 0.001] }}>
+        <CameraManager />
+        <Environment preset="sunset" />
+        <ambientLight color="pink" />
+        <Avatar
+          position={[0, -1.75, -4]}
+          rotation-x={degToRad(0)}
+          scale={1.6}
+          pos={pos}
+          move={move}
+          setMove={setMove}
+        />
+        <Gltf
+          src="/models/classroom_default.glb"
+          position={[0.3, -1.7, -1]}
+          onClick={(e) => {
+            setPos({ x: e.point.x, y: 0, z: e.point.z });
+            setMove(true);
+          }}
+        />
+      </Canvas>
     </>
   );
+};
+
+const CameraManager = () => {
+  return <CameraControls minZoom={1} maxZoom={3} />;
 };
